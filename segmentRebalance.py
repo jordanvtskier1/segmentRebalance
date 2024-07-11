6f3900f81f6eb6e1ec8a607d790d0c5cf352c671
HB3245 - Jordan <jgottlieb@nlgcap.com>
2024-07-11 17:13:18 -0400

    Start refactor - add model

--- segmentRebalance.py
+++ segmentRebalance.py
@@ -40,26 +40,127 @@ totalMovesPercent = 5
 runtime = 600
 
 
-# Load assets as df
-path_universal = Path(filepath)
-df = pd.read_excel(path_universal, sheet_name=dataSheetName)
-# Fill missing values with average of column. This method is not perfect and can be updated later
-df['effective_duration'] = df['effective_duration'].fillna(value=df['effective_duration'].mean())
+def loadData(cols:list):
+    # Load assets as df
+    path_universal = Path(filepath)
+    df = pd.read_excel(path_universal, sheet_name=dataSheetName)
+    # Fill missing values with average of column. This method is not perfect and can be updated later
+    df['effective_duration'] = df['effective_duration'].fillna(value=df['effective_duration'].mean())
 
-# Load target funding as funding
-funding = pd.read_excel(path_universal, sheet_name=fundingSheetName)
+    # Load target funding as funding
+    funding = pd.read_excel(path_universal, sheet_name=fundingSheetName)
 
-# Helper function: convert excel column letter to int
-excelColNum = lambda a: 0 if a.upper() == '' else ord(a[-1].upper()) - ord('A') + 26 * excelColNum(a[:-1].upper())
-columns = [excelColNum(col) for col in columns]
+    # Helper function: convert excel column letter to int
+    excelColNum = lambda a: 0 if a.upper() == '' else ord(a[-1].upper()) - ord('A') + 26 * excelColNum(a[:-1].upper())
+    columns = [excelColNum(col) for col in cols]
 
-funding = funding.iloc[rows, columns].rename(
-    columns={funding.columns[columns[0]]: "segment", funding.columns[columns[1]]: "desiredlvl"})
+    funding = funding.iloc[rows, columns].rename(
+        columns={funding.columns[columns[0]]: "segment", funding.columns[columns[1]]: "desiredlvl"})
 
-# Filter portfolio for only segments selected for balancing
-df = df[df["segment"].isin(segments)].reset_index()
-df["uniqueid"] = df.index
-funding = funding[funding["segment"].isin(segments)].reset_index()
+    # Filter portfolio for only segments selected for balancing
+    df = df[df["segment"].isin(segments)].reset_index()
+    df["uniqueid"] = df.index
+    funding = funding[funding["segment"].isin(segments)].reset_index()
 
-#
-assetClassList = list(df['mandate_level_2'].value_counts().index)
+    return df, funding
+
+
+def runOptimizer(df, funding):
+    # Get list of asset types from data
+    assetClassList = list(df['mandate_level_2'].value_counts().index)
+
+    # Create LpProblem object
+    prob = pulp.LpProblem("Balance_Segments", pulp.LpMinimize)
+    # Define decision variables
+    asset_vars = pulp.LpVariable.dicts("asset",
+                                   ((i, j) for i in df.index for j in funding['segment']),
+                                   cat='Binary')
+    # Variable to minimize. Equal to segment value difference from desired value
+    segments_diff = pulp.LpVariable.dicts('segments_diff', segments, cat='Continuous')
+
+    # Create dictionary with current asset allocation
+    asset_set = {}
+    for i in df.index:
+        realalloc = df.loc[i,'segment']
+        for j in funding['segment']:
+            if j == realalloc:
+                asset_set[(i,j)] = 1
+            else:
+                asset_set[(i,j)] = 0
+
+    # CONSTRAINTS:
+
+    # Only move assets out of specified segment(s). If overFundSegments is empty this will skip
+    if overFundSegments:
+        for i in df[~df['segment'].isin(overFundSegments)].index:
+            for j in segments:
+                prob += asset_vars[i, j] == asset_set[(i,j)]
+
+    # Only move below totalMovesPercent of loan
+    prob += pulp.lpSum([asset_vars[i, j] * asset_set[(i,j)]
+                        for i in df.index
+                        for j in funding['segment']]) >= int(len(df)*(1-(totalMovesPercent/100)))
+
+    # Each loan can only be assigned to one segment
+    for i in df.index:
+        prob += pulp.lpSum(asset_vars[i, j] for j in funding['segment']) == 1
+
+    for j in funding['segment']:
+        # define absolute value of difference using
+        # z[i] >= Ax[i] - By[i]
+        # z[i] >= -(Ax[i] - By[i]) method
+        prob += segments_diff[j] >= (
+                    pulp.lpSum([df.loc[i, 'bv_gaap'] * asset_vars[(i, j)] for i in df.index]) - getDesiredFunding(
+                j)) / getDesiredFunding(j)
+
+        prob += segments_diff[j] >= (getDesiredFunding(j) - pulp.lpSum(
+            [df.loc[i, 'bv_gaap'] * asset_vars[(i, j)] for i in df.index])) / getDesiredFunding(j)
+
+        # Constraints: WA Yield stays nearly constant
+        prob += pulp.lpSum([df.loc[i, 'by_gaap'] * df.loc[i, 'bv_gaap'] * asset_vars[(i, j)]
+                            for i in df.index]) >= getWAyield(j) * 0.995 * pulp.lpSum(
+            [df.loc[i, 'bv_gaap'] * asset_vars[(i, j)] for i in df.index])
+        prob += pulp.lpSum([df.loc[i, 'by_gaap'] * df.loc[i, 'bv_gaap'] * asset_vars[(i, j)]
+                            for i in df.index]) <= getWAyield(j) * 1.005 * pulp.lpSum(
+            [df.loc[i, 'bv_gaap'] * asset_vars[(i, j)] for i in df.index])
+
+        # Constraints: WA duration stays nearly constant
+        prob += pulp.lpSum([df.loc[i, 'effective_duration'] * df.loc[i, 'mv'] * asset_vars[(i, j)]
+                            for i in df.index]) >= getWAduration(j) * 0.995 * pulp.lpSum(
+            [df.loc[i, 'mv'] * asset_vars[(i, j)] for i in df.index])
+        prob += pulp.lpSum([df.loc[i, 'effective_duration'] * df.loc[i, 'mv'] * asset_vars[(i, j)]
+                            for i in df.index]) <= getWAduration(j) * 1.005 * pulp.lpSum(
+            [df.loc[i, 'mv'] * asset_vars[(i, j)] for i in df.index])
+
+        # Constraints: asset class allocation stays nearly constant
+        for assetclass in assetclasslist:
+            prob += pulp.lpSum([df.loc[i, 'bv_gaap'] * asset_vars[(i, j)]
+                                for i in df[df['mandate_level_2'] == assetclass].index]) >= getAllocation(j,
+                                                                                                          assetclass) * pulp.lpSum(
+                [df.loc[i, 'bv_gaap'] * asset_vars[(i, j)]
+                 for i in df.index]) * 0.98
+
+            prob += pulp.lpSum([df.loc[i, 'bv_gaap'] * asset_vars[(i, j)]
+                                for i in df[df['mandate_level_2'] == assetclass].index]) <= getAllocation(j,
+                                                                                                          assetclass) * pulp.lpSum(
+                [df.loc[i, 'bv_gaap'] * asset_vars[(i, j)]
+                 for i in df.index]) * 1.02
+
+    # Objective: minimize sum of absolute value of percent difference from target funding
+    prob += pulp.lpSum(segments_diff[j] for j in segments)
+
+    prob.solve(PULP_CBC_CMD(timeLimit=runtime))
+
+    # Output the results
+    for v in prob.variables():
+        print(v.name, "=", v.varValue)
+
+    # Get the new asset distribution
+    for i in df.index:
+        for j in funding['segment']:
+            if asset_vars[(i, j)].varValue == 1:
+                df.loc[i, "newSegment"] = j
+                if df.loc[i, "segment"] == j:
+                    df.loc[i, "equal"] = 1
+                else:
+                    df.loc[i, "equal"] = 0
